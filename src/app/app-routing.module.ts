f6aaa7fe7a7c6f3cd6f6afd07d80d738d1289afd
angelmrt <amartinr03@gmail.com>
2025-04-08 16:15:51 +0200

    08/04/2025 header a media cocinar y modal de login en el mismo estado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent, pathMatch: 'full' }, // Asigna el componente correctamente
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
