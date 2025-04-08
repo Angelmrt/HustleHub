28915c48ceb9afbcc6162fd0bfae89bbe56c94e6
LENOVO-LMBARREI\amartin <amartin@madrija.com>
2025-04-08 13:37:36 +0200

    08/04/2025 posible solucion a error de no iniciar la aplicacion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
     authDomain: "hustlehub-55746.firebaseapp.com",
     databaseURL: "https://hustlehub-55746-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "hustlehub-55746",
-    storageBucket: "hustlehub-55746.firebasestorage.app",
+ // storageBucket: "hustlehub-55746.firebasestorage.app", Induce a errores si no se utiliza el storage de firebase
     messagingSenderId: "582093535603",
     appId: "1:582093535603:web:bc1672128f7eb3831c2d06",
     measurementId: "G-XHZ77Z1G61"
