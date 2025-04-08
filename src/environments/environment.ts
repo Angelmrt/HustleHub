090d2a9f4a2369f22dc3d2313406b57e415b0077
LENOVO-LMBARREI\amartin <amartin@madrija.com>
2025-04-08 11:11:50 +0200

    08/04/2025 SDK firebase added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-
+  firebaseConfig : {
+    apiKey: "AIzaSyCBIIVrdprkiZ3AmW-HjxZP-wMPzMKfjME",
+    authDomain: "hustlehub-55746.firebaseapp.com",
+    databaseURL: "https://hustlehub-55746-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "hustlehub-55746",
+    storageBucket: "hustlehub-55746.firebasestorage.app",
+    messagingSenderId: "582093535603",
+    appId: "1:582093535603:web:bc1672128f7eb3831c2d06",
+    measurementId: "G-XHZ77Z1G61"
+  }
 };
 
 /*
